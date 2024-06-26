4577cfdb41be5ccd2de1e2987f63473582e66c65
fatemehzakeri <65442130+fatemehzakeri@users.noreply.github.com>
2024-06-26 13:31:32 +0200

    Update Demo.m

--- SnowWE_Estimation/Demo.m
+++ SnowWE_Estimation/Demo.m
@@ -7,7 +7,8 @@ clear                        % Deletes all variables from the workspace, freeing
 close all                    % Closes all figures (windows) that are currently open.
 
 %% Reading the data needed for ranking learning dates using "KNNSnowGeneration" Function
-QueryDates=load('./Data/QueryDates.mat'); % Loads data from the file 'QueryDates.mat' into a structure.
+% Replace '.' with the actual path to your directory
+QueryDates=load('./Data/QueryDates.mat');  % Loads data from the file 'QueryDates.mat' into a structure.
 QueryDates=QueryDates.QueryDates;  % Extracts the 'QueryDates' table from the structure.
 LearningDates=load('./Data/LearningDates.mat'); % Loads data from 'LearningDates.mat' into a structure.
 LearningDates=LearningDates.LearningDates; % Extracts the 'LearningDates' table from the structure.
@@ -16,12 +17,14 @@ Weights=Weights.BayesResultXAtMinObjective;     % Extracts the weights table fro
 
 %% The function for Ranking the learning dates
 clc                          % Clears the Command Window again.
+% Replace '.' with the actual path to your directory
 [ResultIndAll,ErrorSTD,ErrorMean]=KNNSnowGeneration(QueryDates,LearningDates,Weights); % Calls 'KNNSnowGeneration' with loaded data to rank learning dates.
 save('./Output/ResultIndAll.mat','ResultIndAll'); % Saves the ranked learning dates to a file.
 save('./Output/ErrorSTD.mat','ErrorSTD');         % Saves the standard deviation of errors to a file.
 save('./Output/ErrorMean.mat','ErrorMean');       % Saves the mean of errors to a file.
 
 %% Loading additional data
+% Replace '.' with the actual path to your directory
 SWE = load('./Data/SWE.mat');                   % Loads data from 'SWE.mat'.
 SWE=SWE.SWE;                             % Extracts the SWE data.
 SWEIndex = load('./Data/SWEIndex.mat');         % Loads data from 'SWEIndex.mat'.
@@ -38,6 +41,7 @@ destinationFolder = './OutputAll';       % Defines a destination folder for savi
 clc                                      % Clears the Command Window one more time.
 
 %% Setting up reference matrix for geospatial data
+% Replace '.' with the actual path to your directory
 info=geotiffinfo('./Data/RasterCal.tif');       % Retrieves metadata from a GeoTIFF file.
 
 R = makerefmat('RasterSize', [225 225], ...% Creates a reference matrix specifying the raster size and geographic limits.
